91b24299c70f659fff267d6a523a35213cc147ea
JosedeJesus_Martinez <jesus_martinez@oxy.com>
2024-06-15 09:43:34 -0500

    init:

---
+++ easy/lc-21.py
@@ -0,0 +1,12 @@
+"""
+Merge two sorted lists
+
+merge two sorted linked lists nd return is aas a nwe sorted list.
+the new list should be made by spliicing together the nodes of the first two lists
+
+
+x = [1,2,4]
+y = [1,3,4]
+
+
+"""
