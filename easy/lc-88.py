fc9ad28acb94b54880bfaa07d53f736000b4497a
Jesus Martinez <mtzgtzjesus@gmail.com>
2024-08-19 14:28:41 -0500

    test: improved version double check to follow the leetcode init solution class

--- easy/lc-88.py
+++ easy/lc-88.py
@@ -6,33 +6,30 @@ given two sorted int arrs, nums1, nums2, merge nums2 INTO nums1 as one sorted ar
 
 from typing import List
 
-class Sol:
-    def __init__(self,nums1: List[int], nums2: List[int]) -> None:
-        self.nums1 = nums1
-        self.nums2 = nums2
-        self.n = len(self.nums2)
-        self.m = len(self.nums1) - self.n
-    def merge(self):
-        m = self.m -1
-        n = self.n-1
-        # get index of last indx of 1
-        last = m + n +1
-        iter = 0
-        while m >= 0 and n >=0:
-            if self.nums1[m] > self.nums2[n]:
-                self.nums1[last] = self.nums1[m]
-                m-=1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place  
+ instead.
+        """
+        last = m + n - 1
+        m -= 1
+        n -= 1
+
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[last] = nums1[m]
+                m -= 1
             else:
-                self.nums1[last] = self.nums2[n]
-                n-=1
-            print(self.nums1)
-            print(iter)
-            iter += 1
-            last -= 1
-        
-        while n>=0:
+                nums1[last] = nums2[n]
+                n -= 1
+            last-= 1
+
+        # Handle remaining elements in nums2
+        while n >= 0:
             nums1[last] = nums2[n]
-            n, last = n-1, last
+            n -= 1
+            last -= 1
         
         
         
